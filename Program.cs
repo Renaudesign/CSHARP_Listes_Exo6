e46ba94090533c0ba5978c9c6825d55e1fd1f4ff
renaudesign <renaud.sourmail@gmail.com>
2021-02-19 12:11:11 +0100

    commentaire inutile

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CSHARP_Listes_Exo6
+{
+    class Program
+    {
+        //création de l'énumération
+        enum week
+                { 
+                    Lundi,
+                    Mardi,
+                    Mercredi,
+                    Jeudi,
+                    Vendredi,
+                    Samedi,
+                    Dimanche
+                }
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Les jours de la semaine sont:");
+            //Boucle permettant pour chaque entrée de l'énum, d'écrire la valeur de l'énum grâce à GetNames
+            foreach (string day in week.GetNames(typeof(week)))
+            {
+                Console.WriteLine(day);
+            }
+            
+
+        }
+    }
+}
